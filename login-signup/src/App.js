33545e1f74a016221b7e4116e745c85f29d435b0
aakash <aakash_kuruva@srmap.edu.in>
2024-02-13 00:23:05 +0530

    Login-signup page

---
+++ login-signup/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Loginsignup from './Components/Login-signup/Loginsignup';
+
+function App() {
+  return (
+    <div >
+      <Loginsignup/>
+    </div>
+  );
+}
+
+export default App;
