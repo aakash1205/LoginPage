83702132e6042d004244f5bd6a4012388b7c6277
aakash <aakash_kuruva@srmap.edu.in>
2024-02-13 00:37:59 +0530

    login signup

--- login-signup/src/Components/Login-signup/Loginsignup.jsx
+++ login-signup/src/Components/Login-signup/Loginsignup.jsx
@@ -4,6 +4,7 @@ import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
+
 const Loginsignup=()=>{
 
     const [action,setAction]=useState("Sign Up")
